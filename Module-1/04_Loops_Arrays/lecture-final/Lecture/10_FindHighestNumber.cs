7ce8881f66084a436be941843d58a5628489f3f9
Mike Morel <mike@techelevator.com>
2021-01-14 15:43:13 -0500

    M1D4 lecture

---
+++ Module-1/04_Loops_Arrays/lecture-final/Lecture/10_FindHighestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureProblem
+    {
+        /*
+         10. What code do we need to write so that we can find the highest
+             number in the array randomNumbers?
+             TOPIC: Looping Through Arrays
+        */
+        public int FindTheHighestNumber(int[] randomNumbers)
+        {
+
+            int currentHighestNumber = randomNumbers[0];
+
+            for (int i = 1; i < randomNumbers.Length; i++)
+            {
+                //if (randomNumbers[i] > currentHighestNumber)
+                //{
+                //    currentHighestNumber = randomNumbers[i];
+                //}
+                currentHighestNumber = Math.Max(currentHighestNumber, randomNumbers[i]);
+            }
+            return currentHighestNumber;
+        }
+    }
+}
---
+++ Module-1/04_Loops_Arrays/lecture-final/Lecture/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Use a for-loop to print "Hello World" 10 times
+
+            // for ( "initialize a varable"; "while this is true, run the loop"; "do this after each time through the loop")
+
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+
+
+            // 2. Create an array of quiz scores
+            int[] scores = new int[] {94, 87, 100, 66, 75};
+
+            // 3. Print all the scores to the screen
+            for (int i = 0; i < scores.Length; i += 1)
+            {
+                Console.WriteLine($"Score = {scores[i]}");
+            }
+
+            // 4. Grade on a curve...increase all scores by the curve amount
+            int curveAmount = 7;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] += curveAmount;
+                Console.WriteLine($"{i+1} Score = {scores[i]}");
+            }
+
+            // 5. Calculate and print the average score for the class after the curve.
+
+            // Create a variable for the total of all scores.  Declare it BEFORE the loop.
+            int runningTotal = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                // In the loop, add each score to the total
+                runningTotal += scores[i];
+            }
+
+            // After the loop is done, divide the total by the length of the array
+            double averageScore = runningTotal / (double)scores.Length;
+
+            Console.WriteLine($"Average score is {averageScore:n2}");
+
+        }
+
+    }
+}
