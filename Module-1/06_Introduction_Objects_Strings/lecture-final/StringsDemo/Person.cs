212c0bc68eae1b98ba33967ec1787186c57aab20
Mike Morel <mike@techelevator.com>
2021-01-18 17:25:14 -0500

    M1D6 lecture

---
+++ Module-1/06_Introduction_Objects_Strings/lecture-final/StringsDemo/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsDemo
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int HeightInches;
+        public DateTime BirthDate;
+    }
+
+}
