1af9e18df2d76611745d13ce33ce15a0215bb5b8
Mike Morel <mike@techelevator.com>
2021-01-22 17:49:58 -0500

    M1D10 lecture

---
+++ Module-1/10_Week2_Review/lecture-final/Demo/Demo/Classes/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Classes
+{
+    public class Student
+    {
+
+        private int nonStaticField = 0;
+        static private int countOfInstances = 0;
+
+        public string Name { get; set; }
+        public List<int> Scores { get; private set; } = new List<int>();
+
+        public double AverageScore
+        {
+            get
+            {
+                if (Scores.Count == 0)
+                {
+                    return 0.0;
+                }
+                double sum = 0;
+                foreach (int score in Scores)
+                {
+                    sum += score;
+                }
+                return sum / Scores.Count;
+            }
+        }
+
+
+        public string SSN { private get; set; }
+
+
+
+        public Student(string name, string ssn)
+        {
+            Name = name;
+            SSN = ssn;
+
+            Student.countOfInstances++;
+            this.nonStaticField++;
+        }
+
+        public void AddScore(int score)
+        {
+            this.Scores.Add(score);
+        }
+
+        public static int GetCurve()
+        {
+            return 14;
+        }
+    }
+}
---
+++ Module-1/10_Week2_Review/lecture-final/Demo/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> csharpClass = new List<Student>();
+
+            Student.GetCurve();
+
+
+
+            Console.WriteLine();
+
+
+            Student person = new Student("Todd", "123456789");
+            person.AddScore(3);
+            person.AddScore(3);
+            person.AddScore(2);
+            person.AddScore(3);
+
+            Console.WriteLine($"{person.Name}'s average score is {person.AverageScore}");
+
+
+            Student s2 = new Student("s2", "111");
+            Student s3 = new Student("s3", "111");
+        }
+    }
+}
