fe7a4e91af1032986b762c620bc434b4c179e5de
Mike Morel <mike@techelevator.com>
2021-01-24 18:11:37 -0500

    M1D11 student

---
+++ Module-1/11_Inheritance/student-lecture/Shapes/Shapes/Models/Shape2D.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Shapes.Models
+{
+    /// <summary>
+    /// A two-dimensional shape that can be drawn on the screen
+    /// </summary>
+    public class Shape2D
+    {
+        #region statics
+        public static char edgeSymbol = '*';
+        public static char fillSymbol = '*'; //'█';
+        protected const double ASPECT_ADJUSTMENT = 0.45;
+        #endregion
+
+        #region Fields
+        // A place to save the current color for restoring after the draw
+        private ConsoleColor savedColor = ConsoleColor.White;
+        #endregion
+
+        #region Properties
+        public ConsoleColor Color { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+
+        #region Public Methods
+
+        #endregion
+
+        #region Helper Methods
+        protected void SetConsoleColor()
+        {
+            this.savedColor = Console.ForegroundColor;
+            Console.ForegroundColor = Color;
+        }
+
+        protected void ResetConsoleColor()
+        {
+            Console.ForegroundColor = savedColor;
+        }
+        #endregion
+    }
+}
+
---
+++ Module-1/11_Inheritance/student-lecture/Shapes/Shapes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Shapes
+{
+
+    // TODO 01 Create a Shape class with the appropriate methods and properties
+    //      Properties: X, Y, Color, IsFilled, Area, Perimeter
+    //      Methods: Draw, ToString
+    // TODO 02 Create a subclass for Circle and provide the appropriate implementations
+    // TODO 03 Create a subclass for Rectangle and provide the appropriate implementations
+
+    class Program
+    {
+        #region This code is advanced code which allows us to Maximize the Console Window on startup
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int MAXIMIZE = 3;
+        #endregion
+
+        /* *
+         * Draw 2D Shapes on a "canvas".  Circle and Rectangle will do the trick, but one can imagine triangles and other polygons.
+         * */
+        static void Main(string[] args)
+        {
+            // Maximize the console
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            DrawingCLI menu = new DrawingCLI();
+            menu.Show();
+
+            Console.WriteLine("Thanks for drawing with us!");
+            Console.ReadKey();
+        }
+    }
+}
