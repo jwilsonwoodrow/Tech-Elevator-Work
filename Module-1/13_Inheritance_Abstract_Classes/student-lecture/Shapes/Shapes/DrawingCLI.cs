51ee1b75fe4154edbac1db6067c51681d78fb78e
Mike Morel <mike@techelevator.com>
2021-01-26 20:28:31 -0500

    M1D13 student

---
+++ Module-1/13_Inheritance_Abstract_Classes/student-lecture/Shapes/Shapes/DrawingCLI.cs
@@ -0,0 +1,164 @@
+﻿using MenuFramework;
+using Shapes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shapes
+{
+    /*************************************************************************
+     * Try this:
+        At (20, 10), a White Rectangle, 70 X 80, filled.
+        At (55, 50), a Red Circle with radius 25, filled.
+        At (10, 10), a DarkGray Rectangle, 8 X 90, filled.
+        At (14, 3), a Blue Circle with radius 8, filled.
+
+     * ***********************************************************************/
+    class DrawingCLI : ConsoleMenu
+    {
+        #region Fields
+        /****************** Polymorphism and Interfaces ********************
+         * Hold IDrawable objects 
+        ********************************************************************/
+        List<IDrawable> drawableThings = new List<IDrawable>();
+        #endregion
+
+        public DrawingCLI()
+        {
+            this.AddOption("Add a Circle", AddCircle)
+                .AddOption("Add a Rectangle", AddRectangle)
+                .AddOption("Add a Label", AddText)
+                .AddOption("Draw the Canvas", DrawCanvas)
+                .AddOption("List All Shapes", ListShapes)
+                .AddOption("Calculate Total Area", ShowTotalArea)
+                .AddOption("Clear the Canvas", ClearCanvas)
+                .AddOption("Quit", Exit, "Q")
+                .Configure(cfg =>
+               {
+                   cfg.Title = "Shapes! Drawing Program";
+                   cfg.ItemForegroundColor = ConsoleColor.Yellow;
+                   cfg.SelectedItemForegroundColor = ConsoleColor.Green;
+                   cfg.MenuSelectionMode = MenuSelectionMode.Arrow;
+               });
+        }
+
+        protected override void OnBeforeShow()
+        {
+            Console.WriteLine("███████╗██╗  ██╗ █████╗ ██████╗ ███████╗███████╗██╗");
+            Console.WriteLine("██╔════╝██║  ██║██╔══██╗██╔══██╗██╔════╝██╔════╝██║");
+            Console.WriteLine("███████╗███████║███████║██████╔╝█████╗  ███████╗██║");
+            Console.WriteLine("╚════██║██╔══██║██╔══██║██╔═══╝ ██╔══╝  ╚════██║╚═╝");
+            Console.WriteLine("███████║██║  ██║██║  ██║██║     ███████╗███████║██╗");
+            Console.WriteLine("╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝");
+        }
+
+        private MenuOptionResult AddCircle()
+        {
+            int x = GetInteger("Center (X): ", null, Enumerable.Range(0, 100));
+            int y = GetInteger("Center (Y): ", null, Enumerable.Range(0, 100));
+            int radius = GetInteger("Radius: ", 1, Enumerable.Range(1, 50));
+            ConsoleColor color = GetColor();
+            bool filled = GetBool("Do you want the shape filled? ", false);
+            drawableThings.Add(new Circle(x, y, color, filled, radius));
+            Console.WriteLine("New Circle was added");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult AddRectangle()
+        {
+            int x = GetInteger("Left (X): ", null, Enumerable.Range(0, 100));
+            int y = GetInteger("Top (Y): ", null, Enumerable.Range(0, 100));
+            int width = GetInteger("Width: ", null, Enumerable.Range(1, 100));
+            int height = GetInteger("Height: ", null, Enumerable.Range(1, 100));
+            ConsoleColor color = GetColor();
+            bool filled = GetBool("Do you want the shape filled? ", false);
+            drawableThings.Add(new Rectangle(x, y, color, filled, width, height));
+            Console.WriteLine("New Rectangle was added");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private ConsoleColor GetColor()
+        {
+            string colorString = GetString("Color: ", false, "White", Enum.GetNames(typeof(ConsoleColor)));
+            ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorString, true);
+            return color;
+        }
+
+        /// <summary>
+        /// Draw all the shapes onto the canvas (Console)
+        /// </summary>
+        private MenuOptionResult DrawCanvas()
+        {
+            // We draw "things that are drawable"
+            foreach (IDrawable drawable in drawableThings)
+            {
+                drawable.Draw();
+            }
+            Console.CursorVisible = false;
+            Console.ReadLine();
+            Console.CursorVisible = true;
+            return MenuOptionResult.DoNotWaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult ListShapes()
+        {
+            // We draw "things that are drawable"
+            Console.WriteLine("Shapes:");
+            foreach (IDrawable drawable in drawableThings)
+            {
+                Console.WriteLine($"\t{drawable}");
+            }
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult ClearCanvas()
+        {
+            drawableThings.Clear();
+
+            Console.WriteLine("Canvas was cleared");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        /********************************************
+        * Method *private MenuOptionResult AddText()*
+        *          1. Prompt the user for the text location (x and y)
+        *          1. Prompt the user for the text label
+        *          2. Prompt the user for the text color
+        *          3. Create the new Text object
+        *          4. Add the new Text object to the list of drawables
+        *          5. Report success to the user
+        ********************************************/
+        private MenuOptionResult AddText()
+        {
+            int x = GetInteger("Left (X): ", null, Enumerable.Range(0, 100));
+            int y = GetInteger("Top (Y): ", null, Enumerable.Range(0, 100));
+            string label = GetString("Label: ");
+            ConsoleColor color = GetColor();
+            drawableThings.Add(new Text(x, y, color, label));
+            Console.WriteLine("New Label was added");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        /********************************************
+        * Method to "Get total area" 
+        *       private MenuOptionResult ShowTotalArea()
+        *   and report it to the user
+        ********************************************/
+        private MenuOptionResult ShowTotalArea()
+        {
+            int totalArea = 0;
+            foreach(IDrawable drawable in drawableThings)
+            {
+                if (drawable is Shape2D)
+                {
+                    Shape2D shape = (Shape2D)drawable;
+                    totalArea += shape.Area;
+                }
+            }
+            Console.WriteLine($"Total area of all shapes is {totalArea}.");
+
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+    }
+}
---
+++ Module-1/13_Inheritance_Abstract_Classes/student-lecture/Shapes/Shapes/Models/Rectangle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Shapes.Models
+{
+    class Rectangle : Shape2D
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public override int Area
+        {
+            get
+            {
+                return Height * Width;
+            }
+        }
+        public override int Perimeter
+        {
+            get
+            {
+                return (2 * Height) + (2 * Width);
+            }
+        }
+
+        #region Constructors
+        public Rectangle(int x, int y, ConsoleColor color, bool isFilled, int width, int height) :
+            base(x, y, color, isFilled)
+        {
+            Width = width;
+            Height = height;
+        }
+        #endregion
+
+        public override void Draw()
+        {
+            SetConsoleColor();
+
+            #region Do the math to calculate which symbols to draw
+
+            // Calculate the top as a ratio of total height of the window
+            int top = (int)(this.Y * Console.WindowHeight / 100.0);
+            int left = (int)(this.X * Console.WindowWidth / 100.0);
+            int heightLines = (int)(this.Height * Console.WindowHeight / 100.0);
+            int widthCharacters = (int)(this.Width * Console.WindowWidth / 100.0);
+            for (int y = 1; y <= heightLines; y++)
+            {
+                Console.CursorTop = top + y;
+                string output;
+
+                // Adjust the width based on the Skew factor
+                if (y == 1 || y == heightLines)
+                {
+                    // first and last line
+                    output = new string(edgeSymbol, widthCharacters);
+                }
+                else
+                {
+                    output = edgeSymbol + new string(IsFilled ? fillSymbol : ' ', Math.Max(widthCharacters - 2, 0)) + edgeSymbol;
+                }
+                Console.CursorLeft = left;
+                Console.Write(output);
+            }
+            #endregion
+
+            ResetConsoleColor();
+        }
+
+        public override string ToString()
+        {
+            return $"At ({X}, {Y}), a {Color} Rectangle, {Width} X {Height},{(IsFilled ? "" : " not")} filled.";
+        }
+
+    }
+}
