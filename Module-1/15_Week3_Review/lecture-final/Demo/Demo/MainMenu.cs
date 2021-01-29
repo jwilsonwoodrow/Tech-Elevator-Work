3f92441a927123fc0d7cc0d923362c4b39dd8639
Mike Morel <mike@techelevator.com>
2021-01-29 16:22:44 -0500

    M1D15 lecture

---
+++ Module-1/15_Week3_Review/lecture-final/Demo/Demo/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MenuFramework;
+namespace Demo
+{
+    public class MainMenu : ConsoleMenu
+    {
+        public MainMenu()
+        {
+            AddOption("Get the current time", GetTime);
+            AddOption("Get the weather", GetWeather);
+            AddOption("Quit", Exit);
+        }
+
+        private MenuOptionResult GetWeather()
+        {
+            Console.WriteLine("Flippin' Cold!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult GetTime()
+        {
+            Console.WriteLine(DateTime.Now);
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+    }
+}
