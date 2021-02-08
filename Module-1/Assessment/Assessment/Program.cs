c788eeab58d589a85af909ef7aac57fb04411be7
Mike Morel <mike@techelevator.com>
2021-02-08 08:40:00 -0500

    Module 1 Assessment

---
+++ Module-1/Assessment/Assessment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Create instances of your object here and call methods.
+
+        }
+    }
+}
