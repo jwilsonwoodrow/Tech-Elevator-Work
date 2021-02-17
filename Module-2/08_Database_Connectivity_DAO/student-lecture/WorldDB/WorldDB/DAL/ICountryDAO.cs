76adebbbd96c0a672d9230c078483d57f55e307a
Mike Morel <mike@techelevator.com>
2021-02-16 21:16:49 -0500

    M2D8 student

---
+++ Module-2/08_Database_Connectivity_DAO/student-lecture/WorldDB/WorldDB/DAL/ICountryDAO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WorldDB.Models;
+
+namespace WorldDB.DAL
+{
+    public interface ICountryDAO
+    {
+        List<Country> GetCountries();
+        List<Country> GetCountries(string continent);
+        Country GetCountry(string code);
+    }
+}
