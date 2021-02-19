28d98c70f9f129c304ebd14d7e2ed5fd607735c0
Mike Morel <mike@techelevator.com>
2021-02-18 20:31:19 -0500

    M2D10 student

---
+++ Module-2/10_Week2_Review/student-lecture-mmorel/WorldDB/WorldDB/DAL/ICountryLanguageDAO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WorldDB.Models;
+
+namespace WorldDB.DAL
+{
+    public interface ICountryLanguageDAO
+    {
+        List<CountryLanguage> GetLanguages(string countryCode);
+    }
+}
