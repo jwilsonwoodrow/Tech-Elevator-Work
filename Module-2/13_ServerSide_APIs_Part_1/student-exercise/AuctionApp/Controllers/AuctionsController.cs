ad5d70d418bb2a4fd6c05ddfb572d9029ee26a6d
Jess Wilson-Woodrow <jwilsonwoodrow@gmail.com>
2021-02-25 10:57:10 -0500

    M2D13 Exercises Complete

--- Module-2/13_ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
+++ Module-2/13_ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
@@ -5,7 +5,7 @@ using AuctionApp.DAO;
 
 namespace AuctionApp.Controllers
 {
-    [Route("[controller]")]
+    [Route("auctions")]
     [ApiController]
     public class AuctionsController : ControllerBase
     {
@@ -21,6 +21,46 @@ namespace AuctionApp.Controllers
             {
                 dao = auctionDao;
             }
+        }
+        //[HttpGet]                                        ... functionality moved to GetAuctionsByTitle()
+        //public List<Auction> GetAllAuctions()
+        //{
+
+        //    List<Auction> auctions = this.dao.List();
+        //    return auctions;
+
+        //}
+
+        [HttpGet("{id}")]
+        public Auction GetAuctionByID(int id)
+        {
+            Auction auction = this.dao.Get(id);
+            return auction;
+        }
+
+        [HttpPost]
+        public Auction PostAuction(Auction auction)
+        {
+            auction = dao.Create(auction);
+            return auction;
+        }
+
+        [HttpGet]
+        public List<Auction> GetAuctions(string title_like = "", double currentBid_lte = 0)
+        {
+            if (string.IsNullOrEmpty(title_like) && currentBid_lte == 0)
+            {
+                return dao.List();
+            }
+            else if (currentBid_lte == 0)
+            {
+                return dao.SearchByTitle(title_like);
+            }
+            else if (string.IsNullOrEmpty(title_like))
+            {
+                return dao.SearchByPrice(currentBid_lte);
+            }
+            else return dao.SearchByTitleAndPrice(title_like, currentBid_lte);
         }
     }
 }
