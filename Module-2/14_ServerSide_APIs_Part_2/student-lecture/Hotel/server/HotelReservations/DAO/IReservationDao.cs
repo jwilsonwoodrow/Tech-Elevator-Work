e95562bd5e0934cf4d90be32b982371159bbef9f
Mike Morel <mike@techelevator.com>
2021-02-24 21:27:10 -0500

    M2D14 student

---
+++ Module-2/14_ServerSide_APIs_Part_2/student-lecture/Hotel/server/HotelReservations/DAO/IReservationDao.cs
@@ -0,0 +1,17 @@
+﻿using HotelReservations.Models;
+using System.Collections.Generic;
+
+namespace HotelReservations.Dao
+{
+    public interface IReservationDao
+    {
+        Reservation Create(Reservation reservation);
+        List<Reservation> FindByHotel(int hotelId);
+        List<Reservation> FindByName(string nameContains);
+        Reservation Get(int id);
+        List<Reservation> List();
+        Reservation Update(Reservation res);
+        bool Delete(int id);
+
+    }
+}
