6157ea63b41be5f82b83b5fba6daf5c8e8c0efe7
Mike Morel <mike@techelevator.com>
2021-02-28 21:16:26 -0500

    M2D16 student

---
+++ Module-2/16_Authentication/student-lecture/HotelReservationsClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using HotelReservationsClient.DAL;
+using HotelReservationsClient.Models;
+using HotelReservationsClient.Views;
+using System;
+using System.Collections.Generic;
+
+namespace HotelReservationsClient
+{
+    class Program
+    {
+        private static readonly string apiUrl = "https://localhost:44322";
+
+        static void Main(string[] args)
+        {
+            IAccountDao accountDao = new AccountApiDao(apiUrl);
+            IHotelDao hotelDao = new HotelApiDao(apiUrl);
+            IReservationDao reservationDao = new ReservationApiDao(apiUrl);
+
+            new MainMenu(accountDao, hotelDao, reservationDao).Show();
+        }
+    }
+}
