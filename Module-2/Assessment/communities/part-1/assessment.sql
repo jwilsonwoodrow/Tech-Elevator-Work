e9c0c136ad383e798f3a84911061a8cbe093492f
Jess Wilson-Woodrow <jwilsonwoodrow@gmail.com>
2021-03-02 09:21:37 -0500

    Assessment Part 1

--- Module-2/Assessment/communities/part-1/assessment.sql
+++ Module-2/Assessment/communities/part-1/assessment.sql
@@ -2,11 +2,23 @@ USE assessment;
 GO
 
 -- Select all the columns from communities from rows that are live (note that "live" is a column in the communities table)
+select * from communities
+where live = 1
 
+select * from properties
 -- Select the name and address from properties where the latitude is less than 0 and were created in the month of June 2019
+select name, address from properties
+where latitude < 0 and Month(created) = 6 
 
 -- Select the name, latitude, and longitude from communities ordered by the created date, oldest first
+select name, latitude, longitude from communities
+order by created  
 
 -- Select created and a count of all the properties created on each date
+select created, count(id) as Count from properties
+group by created
 
 -- Select a community name, live and the address of each property for every community created on or after Oct. 1st, 2019
+select c.Name, live, address from communities c
+join properties p on p.community_id = c.id
+where c.created > '2019/8/1'
