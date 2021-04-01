7a405e285b9d0aea8a29776b0edaad306a88a6d0
Mike Morel <mike@techelevator.com>
2021-03-31 20:23:35 -0400

    M3D19 student

---
+++ Module-3/19_Web_Services_POST/student-lecture/Boards/src/views/AddCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Add New Card</h1>
+    <card-form v-bind:boardId="parseInt($route.params.boardId)" />
+  </div>
+</template>
+
+<script>
+import CardForm from "@/components/CardForm";
+
+export default {
+  components: {
+    CardForm
+  }
+};
+</script>
---
+++ Module-3/19_Web_Services_POST/student-lecture/Boards/src/views/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <card-detail v-bind:card-id="parseInt($route.params.cardId)"/>
+</template>
+
+<script>
+import CardDetail from '@/components/CardDetail';
+
+export default {
+  components: {
+    CardDetail
+  }
+};
+</script>
