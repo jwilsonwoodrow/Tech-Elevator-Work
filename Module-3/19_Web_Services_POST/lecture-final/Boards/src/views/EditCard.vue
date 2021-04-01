cb575c1685703a029cd762359798fd2e1664d6f5
Mike Morel <mike@techelevator.com>
2021-04-01 14:52:26 -0400

    M3D19 lecture

---
+++ Module-3/19_Web_Services_POST/lecture-final/Boards/src/views/EditCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Edit Card</h1>
+    <card-form v-bind:cardId="parseInt($route.params.cardId)" v-bind:boardId="parseInt($route.params.boardId)" />
+  </div>
+</template>
+
+<script>
+import CardForm from "@/components/CardForm";
+
+export default {
+  components: {
+    CardForm
+  }
+};
+</script>
